bb54769ce09902582a2768f02282bca214557b3b
Chaoskaefer <jonathan.stalling@gmx.de>
2025-10-27 18:00:46 +0100

    Added service worker for pwa and changed icon

---
+++ docs/js/service-worker.js
@@ -0,0 +1,67 @@
+ 
+// The version of the cache.
+ const VERSION = "v1";
+
+ const CACHE_NAME = "simplyDMX_${VERSION}";
+ 
+// The files to make available for offline use.
+const URLS = [    
+    "/",
+    "/docs/index.html",
+    "/docs/style.css",
+    "/docs/js/addressCalc.js",
+    "/docs/js/footprintCalc.js"
+];
+
+//save data to cache on install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(URLS);
+    })(),
+  );
+});
+
+
+// Refresh pwa and clear old cache data 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+          return undefined;
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
+
+// On fetch, intercept server requests
+// and respond with cached responses instead of going to network
+self.addEventListener("fetch", (event) => {
+  // As a single page app, direct app to always go to cached home page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  // For all other requests, go to the cache first, and then the network.
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // If resource isn't in the cache, return a 404.
+      return new Response(null, { status: 404 });
+    })(),
+  );
+});
